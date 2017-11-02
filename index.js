848688638b2360cd6add2b8101dc63e19f77caba
Ryan Birmingham <birm@rbirm.us>
2017-11-02 17:07:42 -0400

    use apis directly

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const cloudrail = require("cloudrail-si");
-cloudrail.Settings.setKey("[Login to see your license key]");
+var rp = require('request-promise');
 
-let service;
+var options = {
+    method: 'POST',
+    uri: 'http://api.posttestserver.com/post',
+    body: {
+        some: 'payload'
+    },
+    json: true // Automatically stringifies the body to JSON
+};
 
-const box = new cloudrail.services.Box(
-    cloudrail.RedirectReceivers.getLocalAuthenticator(8082),
-    "[Box Client Identifier]",
-    "[Box Client Secret]",
-    "http://localhost:8082/auth",
-    "someState"
-);
-
-const dropbox = new cloudrail.services.Dropbox(
-    cloudrail.RedirectReceivers.getLocalAuthenticator(8082),
-    "[Dropbox Client Identifier]",
-    "[Dropbox Client Secret]",
-    "http://localhost:8082/auth",
-    "someState"
-);
-
-// 'selection' is a String representing e.g. a user's service choice
-switch(selection) {
-    case "box": service = box; break;
-    case "dropbox": service = dropbox; break;
-}
-
-service.download(
-    "/myFolder/myFile.png",
-    (error, result) => {
-        // Check for potential error and use the result
-    }
-);
+rp(options)
+    .then(function (parsedBody) {
+        // POST succeeded...
+    })
+    .catch(function (err) {
+        // POST failed...
+    });
